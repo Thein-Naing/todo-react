620319d6ee417a4b9161a686c07bdb9fd83a9d15
Thein Naing <theinnaing0908@gmail.com>
2023-06-04 10:31:43 +0800

    update css styling

--- src/App.js
+++ src/App.js
@@ -15,27 +15,23 @@ function App() {
       "reminder": true,
     },
 
-
-
-
     {
       "id": 2,
-      "text": "Doctors Appointment",
-      "day": "Feb 5th at 2:30pm",
+      "text": "Sleep, Drive, Eat , Code, Dream, Repeat",
+      "day": "Everyday 4am-11pm",
       "reminder": true,
     },
     {
       "id": 3,
-      "text": "Meeting at School",
-      "day": "Feb 6th at 1:30pm",
+      "text": "Sleep, Drive, Eat , Code, Dream, Repeat",
+      "day": "Everyday 4am-11pm",
       "reminder": true,
     },
 
     {
-      "id": 4,
-      "text": "Food Shopping",
-      "day": "Feb 5th at 2:30pm",
-      "reminder": false,
+      "text": "Sleep, Drive, Eat , Code, Dream, Repeat",
+      "day": "Everyday 4am-11pm",
+      "reminder": true,
     },
 
   ]);
